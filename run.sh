e947df017332e9f3cd9294e3238b5f0a94fb81e7
songbinliu <songbin.liu@turbonomic.com>
2017-06-26 13:53:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,6 @@ slave2="ip-172-23-1-12.us-west-2.compute.internal"
 nodeName=$slave1
 
 options="$options --kubeConfig $k8sconf "
-#options="$options --v 3 --logtostderr "
 options="$options --v 3 "
 options="$options --nameSpace $nameSpace"
 options="$options --podName $podName "
